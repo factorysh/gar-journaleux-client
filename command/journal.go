e3ac22e83f04aa90222957751b4c8cbde2915ef1
Wilfried OLLIVIER <wollivier@bearstech.com>
2017-07-06 17:53:20 +0200

    Plug client config into main program

--- command/journal.go
+++ command/journal.go
@@ -6,6 +6,7 @@ import (
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/urfave/cli"
 	gl_rpc "gitlab.bearstech.com/factory/gitlab-authenticated-rpc/rpc"
+	"gitlab.bearstech.com/factory/journaleux/client/conf"
 	"gitlab.bearstech.com/factory/journaleux/rpc"
 	"io"
 	"log"
@@ -14,6 +15,14 @@ import (
 )
 
 func (c *Client) Journal(_cli *cli.Context) error {
+	// read from config file
+
+	var config conf.ClientConfig
+	err := conf.ReadClientConf(&config, ".journaleux.toml")
+	if err != nil {
+		return errors.New("Error reading client config file")
+	}
+
 	if _cli.Int("lines") >= 0 && _cli.Bool("follow") {
 		return errors.New("Error, impossible to both print n lines and follow")
 	}
@@ -26,7 +35,14 @@ func (c *Client) Journal(_cli *cli.Context) error {
 			return err
 		}
 	}
-	err := c.SetDomain(_cli.GlobalString("domain"))
+
+	// use conf but first, overwrite stuff with context pass via cmd line
+	// flags and arguments > config file
+	if _cli.Args().First() != "" {
+		config.Project.Name = _cli.Args().First()
+	}
+
+	err = c.SetDomain(_cli.GlobalString("domain"))
 	if err != nil {
 		return err
 	}
@@ -37,7 +53,7 @@ func (c *Client) Journal(_cli *cli.Context) error {
 		return err
 	}
 	tail, err := j.Tail(c.Ctx, &rpc.Predicate{
-		Project: _cli.Args().First(),
+		Project: config.Project.Name,
 		Lines:   int32(_cli.Int("lines")),
 		Follow:  _cli.Bool("follow"),
 		Regexp:  _cli.String("regexp"),
